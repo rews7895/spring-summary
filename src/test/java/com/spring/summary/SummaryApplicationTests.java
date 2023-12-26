231aae91f9976e70da61ecabc2ca1530b034beb3
Junseok <yalvz@naver.com>
2023-12-27 00:01:51 +0900

    first commit

---
+++ src/test/java/com/spring/summary/SummaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.summary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
